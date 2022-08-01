c035e23040ae26ef7f008383d597a3ba9b8fec46
Elfin Git <musongoletracy@gmail.com>
2022-08-01 23:16:39 +0300

    Update readme

--- index.js
+++ index.js
@@ -5,63 +5,62 @@ const author = document.getElementById('author');
 const bookContainer = document.querySelector('.added-books');
 const newDiv = document.createElement('div');
 newDiv.classList.add('my-list');
-bookContainer.appendChild(newDiv)
+bookContainer.appendChild(newDiv);
 const form = document.getElementsByTagName('form');
 
 const newBook = (title, author) => {
-    const objectData = {
-        bookTitle: title,
-        bookAuthor: author
-    }
-    bookArr.push(objectData);
-    localStorage.setItem('book', JSON.stringify(bookArr));
-    newDiv.innerHTML += `<div>
+  const objectData = {
+    bookTitle: title,
+    bookAuthor: author,
+  };
+  bookArr.push(objectData);
+  localStorage.setItem('book', JSON.stringify(bookArr));
+  newDiv.innerHTML += `<div>
     <p><strong>${objectData.bookTitle}</strong></p>
     <p><strong>${objectData.bookAuthor}</strong></p>
     <button class="remove">Remove</button>
     <hr>
     </div>
-    `
-    form.reset();
+    `;
 };
 
 const removeBooks = () => {
-    newDiv.addEventListener('click', (e) => {
-        if (e.target.classList.contains('remove')) {
-            let list = e.target.parentElement;
-            let bookTitle = list.childNodes[4].value;
-            let remain = bookArr.filter((book) => book.bookTitle !== bookTitle);
-            localStorage.setItem('book', JSON.stringify(remain));
-            newDiv.removeChild(list);
-        }
-    })
-}
+  newDiv.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove')) {
+      const list = e.target.parentElement;
+      const bookTitle = list.childNodes[4].value;
+      const remain = bookArr.filter((book) => book.bookTitle !== bookTitle);
+      localStorage.setItem('book', JSON.stringify(remain));
+      newDiv.removeChild(list);
+    }
+  });
+};
 removeBooks();
-//Function to get books from Local Storage
+// Function to get books from Local Storage
 const getDataFromStore = () => {
-    window.addEventListener('load', () => {
-        if (localStorage.getItem('book')) {
-            const books = JSON.parse(localStorage.getItem('book'));
-            books.forEach((data) => {
-                newDiv.innerHTML += `<div>
+  window.addEventListener('load', () => {
+    if (localStorage.getItem('book')) {
+      const books = JSON.parse(localStorage.getItem('book'));
+      books.forEach((data) => {
+        newDiv.innerHTML += `<div>
               <p><strong>${data.bookTitle}</strong></p>
               <p><strong>${data.bookAuthor}</strong></p>
               <button class="remove">delete</button>
               <hr/>
               </div>`;
-                bookArr.push(books);
-            });
-        }
-    });
+        bookArr.push(books);
+      });
+    }
+  });
 };
 getDataFromStore();
 
 // Call new books function
 button.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (title.value === "" && author.value === "") {
-        alert('Please fill in all the fields');
-    } else {
-        newBook(title.value, author.value)
-    }
+  e.preventDefault();
+  if (title.value === '' && author.value === '') {
+    alert('Please fill in all the fields');
+  } else {
+    newBook(title.value, author.value);
+  }
 });
