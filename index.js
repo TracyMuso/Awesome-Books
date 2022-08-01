d1e120941c6a727fe7779d7f69cba409200683e7
kidde60 <kgeorgewilliam60@gmail.com>
2022-08-01 16:10:42 +0300

    Add new books container

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ const newDiv = document.createElement('div');
 newDiv.classList.add('my-list');
 bookContainer.appendChild(newDiv);
 const form = document.getElementsByTagName('form');
+
+const newBook = (title, author) => {
+    const objectData = {
+        bookTitle: title,
+        bookAuthor: author
+    }
+    bookArr.push(objectData);
+    localStorage.setItem('book', JSON.stringify(bookArr));
+    newDiv.innerHTML += `<div>
+    <p><strong>${objectData.bookTitle}</strong></p>
+    <p><strong>${objectData.bookAuthor}</strong></p>
+    <button class="remove">Remove</button>
+    <hr>
+    </div>
+    form.reset();
+    `
+};
