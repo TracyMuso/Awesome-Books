b134013d4d1eb3833b888c3251912b00cda4a938
Elfin Git <musongoletracy@gmail.com>
2022-08-01 23:35:11 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,61 +5,48 @@ const author = document.getElementById('author');
 const bookContainer = document.querySelector('.added-books');
 const newDiv = document.createElement('div');
 newDiv.classList.add('my-list');
-bookContainer.appendChild(newDiv)
-const form = document.getElementsByTagName('form');
+bookContainer.appendChild(newDiv);
 
 const newBook = (title, author) => {
-    const objectData = {
-        bookTitle: title,
-        bookAuthor: author
-    }
-    bookArr.push(objectData);
-    localStorage.setItem('book', JSON.stringify(bookArr));
-    newDiv.innerHTML += `<div>
+  const objectData = {
+    bookTitle: title,
+    bookAuthor: author,
+  };
+  bookArr.push(objectData);
+  localStorage.setItem('book', JSON.stringify(bookArr));
+  newDiv.innerHTML += `<div>
     <p><strong>${objectData.bookTitle}</strong></p>
     <p><strong>${objectData.bookAuthor}</strong></p>
     <button class="remove">Remove</button>
     <hr>
     </div>
-    `
-
+    `;
 };
 // Add new books function
 button.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (title.value === "" && author.value === "") {
-        alert('Please fill in all the fields');
-    } else {
-        newBook(title.value, author.value)
-
-    }
-    e.preventDefault();
-    if (title.value === "" && author.value === "") {
-        alert('Please fill in all the fields');
-    } else {
-        newBook(title.value, author.value)
-
-    }
-
+  e.preventDefault();
+  if (title.value === '' && author.value === '') {
+    alert('Please fill in all the fields');
+  } else {
+    newBook(title.value, author.value);
+  }
 });
 // Remove book function
 const removeBooks = () => {
-    newDiv.addEventListener('click', (e) => {
-        if (e.target.classList.contains('remove')) {
-            let list = e.target.parentElement;
-            let bookTitle = list.childNodes.value;
-            let remain = bookArr.filter((book) => book.bookTitle !== bookTitle);
-            localStorage.setItem('book', JSON.stringify(remain));
-            newDiv.removeChild(list);
+  newDiv.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove')) {
+      const list = e.target.parentElement;
+      const bookTitle = list.childNodes.value;
+      const remain = bookArr.filter((book) => book.bookTitle !== bookTitle);
+      localStorage.setItem('book', JSON.stringify(remain));
+      newDiv.removeChild(list);
+    }
+  });
+};
 
-        }
-    })
-}
 removeBooks();
-//Function to get books from Local Storage
+// Function to get books from Local Storage
 const getDataFromStorage = () => {
-    localStorage.getItem('book')
+  localStorage.getItem('book');
 };
 getDataFromStorage();
-
-
