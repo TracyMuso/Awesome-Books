46c1a1c26bf5c3f1e748863ca860f0cfdf398316
Elfin Git <musongoletracy@gmail.com>
2022-08-04 17:45:20 +0300

    Style pages and add date

--- index.js
+++ index.js
@@ -69,3 +69,18 @@ form.addEventListener('submit', (e) => {
     library.addBook(newBook);
   }
 });
+
+// Show date and time
+window.onload = () => {
+  const date = new Date();
+  date.setDate(date.getDate());
+  document.querySelector('.date').innerHTML = date;
+
+  function updateTime() {
+    const time = document.getElementsByClassName('date');
+    const datestring = new Date().toLocaleString();
+    const newString = datestring.replace(', ', ' -');
+    time.textContent = newString;
+  }
+  setInterval(updateTime, 1000);
+};
