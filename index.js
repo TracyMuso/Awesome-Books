6af582ea44d87351d019175fcb1f5ed3287784b4
kidde60 <kgeorgewilliam60@gmail.com>
2022-08-05 00:57:46 +0300

    Fix linters

--- index.js
+++ index.js
@@ -11,15 +11,10 @@ class Book {
 
 // UI class to handle all UI events
 class UI {
-  static showBooks(bookArr) {
+  static showBooks(collection) {
     const table = document.querySelector('.table');
     table.innerHTML = '';
-    // edit
-    if (this.collection.addBook().length === 0) {
-    table.innerHTML = '<p class="empty">No books in your collection</p>';
-    }
-    // edit
-    bookArr.forEach((book, index) => {
+    collection.forEach((book, index) => {
       const row = document.createElement('tr');
       const td1 = document.createElement('p');
       td1.innerHTML = `<strong>"${book.title}" by ${book.author}</strong>`;
@@ -41,18 +36,30 @@ class UI {
 class Library {
   constructor() {
     this.collection = JSON.parse(localStorage.getItem('books')) || [];
+    this.checkLength();
+  }
+
+  checkLength() {
+    const empty = document.querySelector('.empty');
+    if (this.collection.length === 0) {
+      empty.innerHTML = 'The library is empty';
+    } else {
+      empty.innerHTML = '';
+    }
   }
 
   addBook(book) {
     this.collection.push(book);
     localStorage.setItem('books', JSON.stringify(this.collection));
     UI.showBooks(this.collection);
+    this.checkLength();
   }
 
   removeBook(id) {
     this.collection = this.collection.filter((book) => book.id !== id);
     localStorage.setItem('books', JSON.stringify(this.collection));
     UI.showBooks(this.collection);
+    this.checkLength();
   }
 }
 
@@ -90,9 +97,8 @@ window.onload = () => {
   setInterval(updateTime, 1000);
 };
 
-
 const homepage = document.querySelector('.home');
-const addNew = document.querySelector('.add')
+const addNew = document.querySelector('.add');
 const contactpage = document.querySelector('.contact');
 const homeSection = document.querySelector('#book-list');
 const bookSection = document.querySelector('#Add-book');
